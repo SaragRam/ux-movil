743579f51c0e6b2881c57e76291d2430901373d0
SaragRam <sg.ramirez940@uniandes.edu.co>
2023-03-13 23:08:28 -0500

    fondo y cambios visuales main, registro y home,

--- HPlusAlarma/app/src/main/java/com/example/hplusalarma/MainActivity.java
+++ HPlusAlarma/app/src/main/java/com/example/hplusalarma/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+
     }
 
     public void registrarse(View view) {
